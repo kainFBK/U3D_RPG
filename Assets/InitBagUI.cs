1ae47561f93ed60e1f7e0e032abd0b3e4d72e9b0
kain <826589265@qq.com>
2018-05-17 21:55:01 +0800

    1.增加背包界面 2.通过grid布局使之可以滑动显示 3.修正界面布局位置
    Signed-off-by: kain <826589265@qq.com>

---
+++ Assets/InitBagUI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InitBagUI : MonoBehaviour {
+
+    int bagsize = 10;
+	// Use this for initialization
+	void Start () {
+
+        GameObject ItemUI = GameObject.Find("ItemPanel");
+        ItemUI.GetComponent<RectTransform>().anchorMax = new Vector2(0.9f, 0.9f);
+        ItemUI.GetComponent<RectTransform>().anchorMin = new Vector2(0.1f, 0.1f);
+        ItemUI.GetComponent<RectTransform>().sizeDelta = new Vector2(0, 0);
+
+        for (int a = 0;a < bagsize; a++)
+        {
+            GameObject b = new GameObject("myimg" + a, typeof(Image));
+            b.GetComponent<Image>().raycastTarget = false;
+            //b.transform.SetParent(Selection.activeTransform);
+            //Selection.activeGameObject = b;
+            b.transform.SetParent(GameObject.Find("ItemPanel").transform);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
